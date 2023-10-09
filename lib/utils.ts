b09fa2232b9531715fe488ac85b357ce9b249134
Hayden Bleasel <hello@haydenbleasel.com>
2023-10-09 16:24:29 -0400

    Start scaffolding

---
+++ lib/utils.ts
@@ -0,0 +1,12 @@
+import { clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+import type { Thing, WithContext } from 'schema-dts';
+import type { ClassValue } from 'clsx';
+
+export const cn = (...inputs: ClassValue[]): string => twMerge(clsx(inputs));
+
+export const toJsonLd = <T extends Thing>(
+  json: WithContext<T>
+): string => `<script type="application/ld+json">
+${JSON.stringify(json, null, 2)}
+</script>`;
--- next.config.js
+++ next.config.js
@@ -1,4 +1,45 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+import { createSecureHeaders } from 'next-secure-headers';
+import withBundleAnalyzer from '@next/bundle-analyzer';
+import { createContentlayerPlugin } from 'next-contentlayer';
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    formats: ['image/avif', 'image/webp'],
+  },
+  experimental: {
+    serverActions: true,
+  },
+  headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: createSecureHeaders({
+          // HSTS Preload: https://hstspreload.org/
+          forceHTTPSRedirect: [
+            true,
+            { maxAge: 63072000, includeSubDomains: true, preload: true },
+          ],
+        }),
+      },
+    ];
+  },
+
+  // Silence, contentlayer
+  webpack: (config) => {
+    config.infrastructureLogging = {
+      level: 'error',
+    };
+
+    return config;
+  },
+};
+
+const withContentlayer = createContentlayerPlugin({
+  // Additional Contentlayer config options
+});
+
+export default process.env.ANALYZE === 'true'
+  ? withBundleAnalyzer()(nextConfig)
+  : withContentlayer(nextConfig);
