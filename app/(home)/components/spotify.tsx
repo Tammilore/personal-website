13477ffc0e37b60b68f89238e33e322656ed1434
Tami <tammilore@gmail.com>
2024-01-07 22:20:23 +0000

    ...

--- app/(home)/components/spotify.tsx
+++ app/(home)/components/spotify.tsx
@@ -1,21 +1,27 @@
 'use client';
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Section } from '@/components/section';
-import { useState, useEffect } from 'react';
 
 function SpotifyPlayer() {
-  const [trackId, setTrackId] = useState('1JhbgwBdzqZ37naFK4agOW'); // Default track ID
+  const [trackId, setTrackId] = useState(null); // Default track ID
 
-  // Fetch the current song every 3 minutes
+  // Fetch the current song immediately when the component mounts, and then every 3 minutes
   useEffect(() => {
-    const interval = setInterval(() => {
+    // Function to fetch current song
+    const fetchSong = () => {
       fetch('/api/spotify')
         .then((response) => response.json())
         .then((data) => {
           setTrackId(data.trackId);
         });
-    }, 30000); // 3 minutes
+    };
+
+    // Fetch the song immediately
+    fetchSong();
+
+    // Then set up interval to fetch the song every 30 seconds
+    const interval = setInterval(fetchSong, 30000); // 30 seconds
 
     // Clean up the interval on unmount
     return () => clearInterval(interval);
@@ -23,16 +29,18 @@ function SpotifyPlayer() {
 
   return (
     <Section title="What I'm currently listening to">
-      <iframe
-        style={{ borderRadius: '12px' }}
-        src={`https://open.spotify.com/embed/track/${trackId}?utm_source=generator`}
-        width="100%"
-        height="352"
-        frameBorder="0"
-        allowFullScreen
-        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-        loading="lazy"
-      />
+      {trackId && (
+        <iframe
+          style={{ borderRadius: '12px' }}
+          src={`https://open.spotify.com/embed/track/${trackId}?utm_source=generator`}
+          width="100%"
+          height="352"
+          frameBorder="0"
+          allowFullScreen
+          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+          loading="lazy"
+        />
+      )}
     </Section>
   );
 }
